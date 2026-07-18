84a1c00fc78a9156bbefde0b0355430001df2d74
Goose40401 <272935001+Goose40401@users.noreply.github.com>
2026-07-18 17:09:29 +0200

    added some functionality

---
+++ scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const GRAVITY = 1200.0;
+const SPEED = 450.0;
+const JUMP_FORCE = 750.0;
+
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta;
+		
+	else: if Input.is_action_just_pressed("jump"):
+		velocity.y = -JUMP_FORCE; 
+	
+	var x = Input.get_axis("left","right");
+	velocity.x = x * SPEED;
+	
+	move_and_slide()
