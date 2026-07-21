c11d2f235d032bd06701cdaedfc0f59dfa442bb8
Goose40401 <272935001+Goose40401@users.noreply.github.com>
2026-07-21 23:00:27 +0200

    added anim (needs optimization)

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,14 +4,18 @@ const GRAVITY = 1200.0;
 const SPEED = 450.0;
 const JUMP_FORCE = 750.0;
 
+@export var animPlayer : AnimationPlayer;
 
 func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta;
+		animPlayer.play("jump");
 		
-	else: if Input.is_action_just_pressed("jump"):
-		velocity.y = -JUMP_FORCE; 
+	else:
+		animPlayer.play("idle");
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -JUMP_FORCE; 
 	
 	var x = Input.get_axis("left","right");
 	velocity.x = x * SPEED;
